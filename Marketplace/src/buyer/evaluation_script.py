3ac17d62e7624dca59cb57123229230ec7c59b52
Swaminathan Sriram <thedarkknight@Swaminathans-MacBook-Pro.local>
2023-02-10 12:24:06 -0700

    Performance evaluation

--- Marketplace/src/buyer/evaluation_script.py
+++ Marketplace/src/buyer/evaluation_script.py
@@ -1,6 +1,7 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
 import os
 import pathlib
+import sys
 
 # path to src directory
 parent_dir = pathlib.Path(__file__).parent.resolve()
@@ -8,11 +9,16 @@ parent_dir = pathlib.Path(__file__).parent.resolve()
 def f1():
     os.system("python3 {0}/execution_script.py |  python3 {1}/buyer-client.py".format(parent_dir,parent_dir)) 
 
-executors_list = []
-to_do = []
-with ThreadPoolExecutor(max_workers=2) as executor:
-    for i in range(1):
-        to_do.append(executor.submit(f1))
+def main():
+    workers = int(sys.argv[1])
+    if not workers:
+        workers = 1
 
-    for future in as_completed(to_do):
-        print("future: {}, result {} ".format(future, future.result()))
+    with ProcessPoolExecutor(max_workers=workers) as executor:
+        #work_list = [i for i in range(workers)]
+        future_to_url = {executor.submit(f1): i for i in range(workers)}
+        for future in as_completed(future_to_url):
+            print("future: {}, result {} ".format(future, future.result()))
+
+if __name__ == '__main__':
+    main()
