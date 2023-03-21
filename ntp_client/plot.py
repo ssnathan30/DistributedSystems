15e488b4c95031b697f29c15796ef0d91aab89bb
Swaminathan Sriram <thedarkknight@Swaminathans-MacBook-Pro.local>
2023-03-20 19:36:43 -0600

    Updating graphs with labels

--- ntp_client/plot.py
+++ ntp_client/plot.py
@@ -12,15 +12,15 @@ def plot(total_delay, total_offset, delay, offset, name, src_dir):
     pyplot.plot([i*8 for i in range(0,len(offset))], offset, label='offset_0')
     pyplot.xlabel("Messages")
     pyplot.ylabel("Delay/Offset")
-    pyplot.title(name)
-    pyplot.savefig(f"{src_dir}/graphs/Graph_{name}.png")
+    pyplot.title(name)    
     pyplot.legend()
+    pyplot.savefig(f"{src_dir}/graphs/Graph_{name}.png")
     #pyplot.show()
 
   
 if __name__ == "__main__":
     src_dir = pathlib.Path(__file__).parent.resolve()
-    filepath = os.getenv("FILE",f"{src_dir}/logs/plotting_data_pool.ntp.org.json")
+    filepath = os.getenv("FILE",f"{src_dir}/logs/plotting_data_localhost.json")
     server_type = os.getenv("SERVER","Local_NTP_Server")
     with open(filepath,"r") as f:
         data = json.loads(f.read())
